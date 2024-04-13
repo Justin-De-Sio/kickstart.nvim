43ec59a4e9a10328d7b7c361ec674d009e399d49
Justin-De-Sio <desio.j@live.fr>
2024-04-13 16:21:17 +0200

    key conflict to fix

--- init.lua
+++ init.lua
@@ -397,6 +397,8 @@ require('lazy').setup({
           --  Similar to document symbols, except searches over your entire project.
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
+          -- FIX: r conflict
+          --
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
